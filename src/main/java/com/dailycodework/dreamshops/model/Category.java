e512bd652f737894e96ff52b1bd22ab9279a34e4
Angga Lesmana <muhidabdul168@gmail.com>
2024-09-14 08:01:14 +0700

    modified response product

--- src/main/java/com/dailycodework/dreamshops/model/Category.java
+++ src/main/java/com/dailycodework/dreamshops/model/Category.java
@@ -19,8 +19,9 @@ public class Category {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "category")
-    private List<Product> products;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 
     public Category(String name) {
         this.name = name;
